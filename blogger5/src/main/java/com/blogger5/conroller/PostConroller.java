6562d2816644b2d7d1333251a25cabf2c066a339
Umesh-Saunshi <umeshsaunshi12@gmail.com>
2024-02-14 20:58:12 +0530

    i done getMappingById

--- blogger5/src/main/java/com/blogger5/conroller/PostConroller.java
+++ blogger5/src/main/java/com/blogger5/conroller/PostConroller.java
@@ -1,5 +1,23 @@
 package com.blogger5.conroller;
 
+import com.blogger5.payload.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/create")
 public class PostConroller {
-    
+
+
+    @GetMapping ("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable Long id){
+       // PostDto post = postService.getPostById(id);
+        return new ResponseEntity<>( HttpStatus.OK);
+
+    }
+
 }
